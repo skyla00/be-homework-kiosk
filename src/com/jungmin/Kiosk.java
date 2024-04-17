804f23349009742790770ef2b3acf51f20aa1bcd
Jungmin Lee <75019459+Lucky-kor@users.noreply.github.com>
2024-04-17 11:03:51 +0900

    Add files via upload

---
+++ src/com/jungmin/Kiosk.java
@@ -0,0 +1,14 @@
+package com.jungmin;
+
+public class Kiosk {
+    private final MenuItem menuItem1 = new MenuItem("김밥", 1000);
+    private final static MenuItem menuItem2 = new MenuItem("계란 김밥", 1500);
+    private final static MenuItem menuItem3 = new MenuItem("충무 김밥", 1000);
+    private final static MenuItem menuItem4 = new MenuItem("떡볶이", 2000);
+
+    // 주문 안내 메시지를 출력하는 메서드를 정의할 수 있습니다.
+    // 주문할 음식을 선택하는 메서드를 정의 할 수 있습니다.
+    // 주문할 음식의 수량을 입력하는 메서드를 정의할 수 있습니다.
+    // 음식 주문을 위한 메서드를 정의할 수 있습니다.
+    // 주문 결과를 출력하는 메서드를 정의할 수 있습니다.
+}
---
+++ src/com/jungmin/KioskApplication.java
@@ -0,0 +1,9 @@
+package com.jungmin;
+
+public class KioskApplication {
+    public static void main(String[] args) {
+        // Kiosk 클래스의 기능을 구현 및 활용해서 프로그래밍 요구 사항에 맞는 키오스크 프로그램을 구현하세요.
+        // MenuItem 클래스는 음식 정보를 표현하고 있으며 MenuItem 클래스를 이용해 주문할 음식 정보를 출력할 수 있습니다.
+        // 이 외의 필요한 클래스가 있다면 자유롭게 추가해서 여러분들만의 키오스크 프로그램을 만들면 됩니다.
+    }
+}
